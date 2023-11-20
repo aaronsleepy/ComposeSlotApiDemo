bc0078431125f34f336498ffece9b9ef1933c04e
Aaron Song <aaron@kmong.com>
2023-11-20 09:44:20 +0900

    ui: Composable 구성 요소 구현

--- app/src/main/java/song/aaron/composeslotapidemo/MainActivity.kt
+++ app/src/main/java/song/aaron/composeslotapidemo/MainActivity.kt
@@ -3,13 +3,26 @@ package song.aaron.composeslotapidemo
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.Checkbox
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import song.aaron.composeslotapidemo.ui.theme.ComposeSlotApiDemoTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,8 +35,82 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
+                    MainScreen()
                 }
             }
         }
     }
+}
+
+@Composable
+fun MainScreen() {
+    var linearSelected by remember {
+        mutableStateOf(true)
+    }
+
+    var imageSelected by remember {
+        mutableStateOf(true)
+    }
+
+    val onLinearClick = { value: Boolean ->
+        linearSelected = value
+    }
+
+    val onTitleClick = { value: Boolean ->
+        imageSelected = value
+    }
+}
+
+@Composable
+fun ScreenContent(
+    linearSelected: Boolean,
+    imageSelected: Boolean,
+    onTitleClick: (Boolean) -> Unit,
+    onLinearClick: (Boolean) -> Unit) {
+
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.SpaceBetween
+    ) {
+    }
+}
+
+@Composable
+fun CheckBoxes(
+    linearSelected: Boolean,
+    imageSelected: Boolean,
+    onTitleClick: (Boolean) -> Unit,
+    onLinearClick: (Boolean) -> Unit) {
+
+    Row(
+        modifier = Modifier.padding(20.dp),
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+
+        Checkbox(
+            checked = imageSelected,
+            onCheckedChange = onTitleClick
+        )
+        Text(text = "Image Title")
+
+        Spacer(modifier = Modifier.width(20.dp))
+
+        Checkbox(
+            checked = linearSelected,
+            onCheckedChange = onLinearClick
+        )
+        Text(text = "Linear Progress")
+
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DemoPreview() {
+    CheckBoxes(
+        linearSelected = true,
+        imageSelected = true,
+        onTitleClick = { /* TODO */ },
+        onLinearClick = { /* TODO */} )
 }
